7770cac9eec7cf7ea10ed58dfd92a702b1d297b4
MariamEwas <maryemhasan2004@gmail.com>
2024-05-09 17:21:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.getElementById("colorButton").addEventListener("click", function() {
+    changeColor();
+});
+
+function changeColor() {
+    var body = document.querySelector("body");
+    body.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
